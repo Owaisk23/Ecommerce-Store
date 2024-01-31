0b9275433d91593f47bc70bb74d362e45466af88
Owais Ahmed Khan <owais1566@gmail.com>
2024-01-31 19:07:53 +0500

    checkout pending

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -2,7 +2,7 @@ import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
 
 import { Link } from 'react-router-dom/dist';
-
+// address se kam krna hai
 const products = [
   {
     id: 1,
@@ -169,7 +169,33 @@ function CheckoutPage() {
               <h2 className="text-base font-semibold leading-7 text-gray-900">Address</h2>
               <p className="mt-1 text-sm leading-6 text-gray-600">
                Choose from existing address</p>
-
+               <ul role="list" className="divide-y divide-gray-100">
+      {address.map((address) => (
+        <li key={person.email} className="flex justify-between gap-x-6 py-5">
+          <div className="flex min-w-0 gap-x-4">
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+            </div>
+          </div>
+          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+            <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+            {person.lastSeen ? (
+              <p className="mt-1 text-xs leading-5 text-gray-500">
+                Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+              </p>
+            ) : (
+              <div className="mt-1 flex items-center gap-x-1.5">
+                <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                  <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                </div>
+                <p className="text-xs leading-5 text-gray-500">Online</p>
+              </div>
+            )}
+          </div>
+        </li>
+      ))}
+    </ul>
               <div className="mt-10 space-y-10">
                 
                 <fieldset>
